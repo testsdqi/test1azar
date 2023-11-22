6037b36e4b93c641c6405fbef2ccc0d09f5dcf7e
testsdqi <perestashop2050@gmail.com>
2023-11-22 08:58:13 +0330

    tedad added

--- test1azar/Program.cs
+++ test1azar/Program.cs
@@ -1,6 +1,8 @@
 ﻿//test
 Console.Write("enter name: ");
 string str = Console.ReadLine();
-for (int i = 0; i < 10; i++) 
+Console.Write("tedad:");
+int a=int.Parse(Console.ReadLine());
+for (int i = 0; i < a; i++) 
 Console.WriteLine(str);
 
