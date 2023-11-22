a9564b5cc7b5cb2264109ab2d01b1c18cb53addf
testsdqi <perestashop2050@gmail.com>
2023-11-22 08:17:27 +0330

    test2 change

--- test1azar/Program.cs
+++ test1azar/Program.cs
@@ -1,5 +1,5 @@
 ﻿Console.Write("enter name: ");
-string a = (Console.ReadLine());
-for (int i = 0; i < 10; i++) ;
+string a = Console.ReadLine();
+for (int i = 0; i < 10; i++) 
 Console.WriteLine(a);
 
